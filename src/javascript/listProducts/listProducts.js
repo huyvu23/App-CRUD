9109e3cb2fb960134cba57a1a1b07f53403d0c27
huyvu23 <vuh32616@gmail.com>
2022-07-11 09:46:15 +0700

    add

--- src/javascript/listProducts/listProducts.js
+++ src/javascript/listProducts/listProducts.js
@@ -29,17 +29,17 @@ let createTableProducts = () => {
                   <td class="text-sm text-black font-light px-6 py-4 whitespace-nowrap">
                   ${item.price} VNĐ
                   </td>
-                  <td class="text-sm text-black font-light px-6 py-4 whitespace-nowrap red">
+                  <td class="text-sm text-black font-light px-6 py-4 whitespace-nowrap">
                   ${item.status}
                   </td>
                   <td class=" px-6 py-4 whitespace-nowrap">
                     <div class="flex">
-                       <button class="md:cursor-pointer">
+                       <button  class="md:cursor-pointer">
                          <svg xmlns="http://www.w3.org/2000/svg" style="color:#1A56DB" class=" h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                          <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                          </svg>   
                        </button>
-                       <button class="md:cursor-pointer">
+                       <button class="md:cursor-pointer btn-delete">
                          <svg xmlns="http://www.w3.org/2000/svg" style="color:#C81E1E" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                          <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                          </svg>
@@ -51,6 +51,7 @@ let createTableProducts = () => {
     }
   });
 };
+
 createTableProducts();
 
 //  ! BUTTON NEXT
@@ -95,3 +96,13 @@ const changePage = () => {
 };
 
 changePage();
+
+const deleteItem = () => {
+  let itemDelete = document.querySelector(".btn-delete");
+  itemDelete.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("click");
+  });
+};
+
+deleteItem();
